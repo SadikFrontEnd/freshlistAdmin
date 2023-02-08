0667b3100ba56d22f3f9d09aa39fab96146ec1ee
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-02-08 10:39:24 +0530

    New Build Pranay

--- build/static/js/274.11ed69da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[274],{2272:function(a,s){}}]);
-//# sourceMappingURL=274.11ed69da.chunk.js.map
